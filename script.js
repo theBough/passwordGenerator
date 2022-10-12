211177f9cda1240f9ea834a0f9b294fcf4d17001
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-10-12 11:17:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var passwordHolder = document.getElementById("showPassword");
 
 function generate(){
   //get a random number from 33 -127
+  //the 96 indicates the range of values.
   var newLetter = Math.random()*96
   //get rid of the decimals
-  newLetter = Math.ceil(newLetter)+33
+  //the number 33 represents what number to start with.
+  newLetter = Math.floor(newLetter)+33
   passwordHolder.innerHTML = String.fromCharCode(newLetter)
 }//end generate
