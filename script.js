613127d47d2bc6eb8762705246557c7631d5b81a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-10-17 10:58:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 var passwordHolder = document.getElementById("showPassword");
 
-function generate(){
+function startHere(){
+  passwordHolder.innerHTML = ""
+  var rndNum;
+  for(i=0 ; i<25 ; i++){
+    rndNum = Math.random()*3
+    rndNum = Math.floor(rndNum)
+    if(rndNum == 0){
+      generateCapitalLetter();
+    }
+    if(rndNum == 1){
+      generateLowerLetter();
+    }
+  }//end loop
+}//end startHere
+
+function generateCapitalLetter(){
   //get a random number from 33 -127
   //the 96 indicates the range of values.
-  var newLetter = Math.random()*96
+  var newLetter = Math.random()*26
   //get rid of the decimals
   //the number 33 represents what number to start with.
-  newLetter = Math.floor(newLetter)+33
-  passwordHolder.innerHTML = String.fromCharCode(newLetter)
+  newLetter = Math.floor(newLetter)+65
+  passwordHolder.innerHTML += String.fromCharCode(newLetter)
 }//end generate
+function generateLowerLetter(){
+  var newLetter = Math.random()*26
+  newLetter = Math.floor(newLetter)+97
+  passwordHolder.innerHTML += String.fromCharCode(newLetter)
+}//end generate
+function generateNumber(){
+  var newLetter = Math.random()*10
+  newLetter = Math.floor(newLetter)+48
+  passwordHolder.innerHTML += String.fromCharCode(newLetter)
+}
